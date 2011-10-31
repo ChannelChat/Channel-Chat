451a7e7d84b2548bfe8e614d61171f21c47723d0
Feildmaster <admin@feildmaster.com>
2011-10-31 04:49:01 -0500

    Beginning Layout of ChannelChat EventFactory

---
+++ src/com/feildmaster/ChanChat/Events/EventFactory.java
@@ -0,0 +1,16 @@
+package com.feildmaster.chanchat.Events;
+
+//!!! TODO: Event Factory
+public class EventFactory {
+    public void CCReloadEvent() {
+    }
+
+    public void CCSaveEvent() {
+    }
+
+    public void ChannelJoinEvent() {
+    }
+
+    public void ChannelCreateEvent() {
+    }
+}
