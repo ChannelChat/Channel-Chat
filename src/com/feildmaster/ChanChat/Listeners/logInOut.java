679d1eb79aa4f0cd99d9eb087a07ab5496b7dadf
Feildmaster <admin@feildmaster.com>
2011-10-31 05:08:45 -0500

    Fixed a bug... Added Notice of active channel on login.

--- src/com/feildmaster/ChanChat/Listeners/logInOut.java
+++ src/com/feildmaster/ChanChat/Listeners/logInOut.java
@@ -3,6 +3,8 @@ package com.feildmaster.chanchat.Listeners;
 import com.feildmaster.chanchat.Chan.Channel;
 import com.feildmaster.chanchat.Chan.ChannelManager;
 import com.feildmaster.chanchat.Chat;
+import com.feildmaster.chanchat.Util.ChatUtil;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
@@ -16,6 +18,8 @@ public class LogInOut extends PlayerListener {
         Player player = event.getPlayer();
         for(Channel chan : cm.getAutoChannels())
             chan.addMember(player);
+        cm.checkActive(player);
+        player.sendMessage(ChatUtil.format(ChatColor.WHITE, ChatColor.YELLOW+"Your active channel is: "+cm.getActiveName(player)));
     }
     public void onPlayerQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
