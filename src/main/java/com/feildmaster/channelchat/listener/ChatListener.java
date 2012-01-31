1ee8f4ed0dbc56140778a6130b07956ad98a086c
Feildmaster <admin@feildmaster.com>
2012-01-31 14:35:22 -0600

    Empty message on #<channel> now sets your active channel. Thanks for the suggestion, dumptruckman.

--- src/main/java/com/feildmaster/channelchat/listener/ChatListener.java
+++ src/main/java/com/feildmaster/channelchat/listener/ChatListener.java
@@ -14,43 +14,51 @@ import static com.feildmaster.channelchat.channel.ChannelManager.getManager;
  * Handles player chat
  */
 public class ChatListener implements Listener {
+
     @EventHandler(priority = EventPriority.LOWEST)
     public void onEarlyPlayerChat(PlayerChatEvent event) {
-        if(event.isCancelled()) return;
+        if (event.isCancelled()) {
+            return;
+        }
         Player player = event.getPlayer();
         String msg = event.getMessage();
 
-        if(getManager().inWaitlist(player)) { // TODO: Modularize
-            Channel chan =getManager().getWaitingChan(player);
-            if(chan.getPass().equals(msg)) {
+        if (getManager().inWaitlist(player)) { // TODO: Modularize
+            Channel chan = getManager().getWaitingChan(player);
+            if (chan.getPass().equals(msg)) {
                 chan.addMember(player);
-                chan.sendMessage(ChatColor.YELLOW+ChatColor.stripColor(player.getDisplayName())+" has joined.");
+                chan.sendMessage(ChatColor.YELLOW + ChatColor.stripColor(player.getDisplayName()) + " has joined.");
                 getManager().deleteFromWaitlist(player);
-            } else if(msg.equalsIgnoreCase("cancel")) {
+            } else if (msg.equalsIgnoreCase("cancel")) {
                 getManager().deleteFromWaitlist(player);
             } else {
-                player.sendMessage(ChatColor.GRAY+"Password incorrect, try again. (cancel to stop)");
+                player.sendMessage(ChatColor.GRAY + "Password incorrect, try again. (cancel to stop)");
             }
             event.setCancelled(true);
         } else if (msg.startsWith("#")) {
             msg = msg.substring(1);
 
             String[] args = msg.split(" ");
+            Channel chan = getManager().getChannel(args[0]);
 
-            if(args.length > 1 && getManager().channelExists(args[0])) {
+            if (args.length > 1 && chan != null) {
                 StringBuilder msg1 = new StringBuilder();
-                for(int x = 1; x<args.length; x++)
-                    msg1.append(msg1.length() == 0?"":" ").append(args[x]);
-                if(msg1.length() > 0 && getManager().getChannel(args[0]).isMember(player))
-                    getManager().sendMessage(player, args[0], msg1.toString());
-                else
+                for (int x = 1; x < args.length; x++) {
+                    msg1.append(msg1.length() == 0 ? "" : " ").append(args[x]);
+                }
+                if (msg1.length() > 0 && chan.isMember(player)) {
+                    getManager().sendMessage(player, chan, msg1.toString());
+                } else {
                     player.sendMessage("Not member of channel");
-            } else if (args.length == 1 && getManager().channelExists(args[0]))
-                player.sendMessage(ChatColor.YELLOW+"Please include a message");
-            else if (args.length >= 1 && !getManager().channelExists(args[0]))
-                player.sendMessage(ChatColor.RED+"Channel not found");
-            else
-                player.sendMessage(ChatColor.RED+"Pleasy specify a channel.");
+                }
+            } else if (args.length == 1 && chan != null) {
+                getManager().setActiveChannel(player, chan);
+                player.sendMessage(info("Now talking in \"" + chan.getName() + ".\""));
+            } else if (args.length >= 1 && chan == null) {
+                player.sendMessage(ChatColor.RED + "Channel not found");
+            } else {
+                player.sendMessage(ChatColor.RED + "Pleasy specify a channel.");
+            }
 
             event.setCancelled(true);
         }
@@ -58,11 +66,13 @@ public class ChatListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerChat(PlayerChatEvent event) {
-        if(event.isCancelled()) return;
+        if (event.isCancelled()) {
+            return;
+        }
 
         Player player = event.getPlayer();
 
-        if(getManager().getJoinedChannels(player).isEmpty()) {
+        if (getManager().getJoinedChannels(player).isEmpty()) {
             player.sendMessage(info("You are not in any channels."));
             event.setCancelled(true);
             return;
@@ -71,7 +81,7 @@ public class ChatListener implements Listener {
         getManager().checkActive(player);
         Channel chan = getManager().getChannel(event);
 
-        if(chan == null) {
+        if (chan == null) {
             plugin().getServer().getLogger().info("[ChannelChat] Error Occured That Shouldn't Happen (chatListener.java)");
             player.sendMessage("[ChannelChat] Error Occured That Shouldn't Happen");
             event.setCancelled(true);
