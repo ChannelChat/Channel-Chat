f1eed2ba3c4e360dbd092f07270b9e722aa3fb10
feildmaster <admin@feildmaster.com>
2012-08-11 09:11:30 -0500

    Fix directories being wrong sometimes... (Why do I hard code?)

--- src/main/java/com/feildmaster/channelchat/configuration/ModuleConfiguration.java
+++ src/main/java/com/feildmaster/channelchat/configuration/ModuleConfiguration.java
@@ -2,12 +2,15 @@ package com.feildmaster.channelchat.configuration;
 
 import com.feildmaster.lib.configuration.EnhancedConfiguration;
 import java.io.File;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Configuration class for simple configuration setting for ChannelChat Plugins
  */
 public class ModuleConfiguration extends EnhancedConfiguration {
+    private static File directory;
+
     /**
      * Creates a new ModuleConfiguration for ChannelChat modules.
      * Uses plugin name for file name
@@ -25,8 +28,12 @@ public class ModuleConfiguration extends EnhancedConfiguration {
      * @param configName The name of the default YamlConfiguration file. (Ex: "PluginConfig.yml")
      */
     public ModuleConfiguration(JavaPlugin p, String configName) {
-        super(new File(new File("plugins/ChannelChat/modules"), configName), p);
+        super(new File(directory, configName), p);
 
         loadDefaults(configName);
+    }
+
+    static {
+        directory = new File(Bukkit.getServer().getPluginManager().getPlugin("ChannelChat").getDataFolder(), "modules");
     }
 }
